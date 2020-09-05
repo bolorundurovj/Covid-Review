1fa7e7675532af2c9bb25bce6647397e35acc4f3
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-09-05 20:10:28 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 let countryList = require('./country_list.json');
 
-mongoose.connect(process.env.URL, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`${process.env.URL}`, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on("error", console.log.bind(console, "connection error"));
 db.once("open", function (callback) {
